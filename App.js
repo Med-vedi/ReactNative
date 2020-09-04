0ec53f710c624c7ae2b7d285565ef3fe23e0477c
Med-vedi <desmopap@gmail.com>
2020-09-04 13:55:02 +0200

    step-1

--- App.js
+++ App.js
@@ -1,21 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  SafeAreaView,
+  Image,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  // console.log(require("./assets/icon.png"));
+  const handlePress = () => console.log("On text press");
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text onPress={handlePress}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis iusto
+        eaque beatae illo autem facilis, debitis repudiandae quaerat tempore
+        fugit dignissimos facere quo sequi expedita! Ab quas eveniet accusantium
+      </Text>
+      <TouchableOpacity onPress={() => console.log("img tapped")}>
+        <Image
+          source={{
+            width: 200,
+            height: 300,
+            uri: "https://picsum.photos/id/237/200/300",
+          }}
+        />
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
