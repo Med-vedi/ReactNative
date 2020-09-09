a8ea404e1424de419f92499a4d53033ff20d0ef2
Med-vedi <desmopap@gmail.com>
2020-09-09 11:57:11 +0200

    text input on hook

--- App.js
+++ App.js
@@ -1,17 +1,29 @@
 import { useDeviceOrientation } from "@react-native-community/hooks";
-import React from "react";
+import React, { useState } from "react";
 import {
   StyleSheet,
   View,
   Platform,
   StatusBar as StatusBarReact,
+  TextInput,
 } from "react-native";
 
 import ListingsScreen from "./app/screens/ListingsScreen";
+import Screen from "./app/components/Screen";
 
 export default function App() {
   const handlePress = () => console.log("On btn press");
-  return <ListingsScreen />;
+  const [firstName, setFirstName] = useState("");
+  console.log(firstName);
+  return (
+    <Screen>
+      <TextInput
+        onChangeText={(text) => setFirstName(text)}
+        placeholder="First Name"
+        style={{ borderBottomColor: "#ccc", borderBottomWidth: 1 }}
+      />
+    </Screen>
+  );
 }
 
 const styles = StyleSheet.create({
