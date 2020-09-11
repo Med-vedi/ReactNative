bf6a33d36cb19373c9c0ed2ab15b23ad36888f52
Med-vedi <desmopap@gmail.com>
2020-09-11 14:21:57 +0200

    ListingEditScreen

--- App.js
+++ App.js
@@ -5,17 +5,16 @@ import {
   StatusBar as StatusBarReact,
 } from "react-native";
 
-import Screen from "./app/components/Screen";
-import AppPicker from "./app/components/AppPicker";
-import AppTextInput from "./app/components/AppTextInput";
-import LoginScreen from "./app/screens/LoginScreen";
-import AppButton from "./app/components/AppButton";
+// import Screen from "./app/components/Screen";
+// import AppPicker from "./app/components/AppPicker";
+// import AppTextInput from "./app/components/AppTextInput";
+// import LoginScreen from "./app/screens/LoginScreen";
+// import AppButton from "./app/components/AppButton";
+import ListingEditScreen from "./app/screens/ListingEditScreen";
+import { AppFormField } from "./app/components/forms";
 
 export default function App() {
-  const [isNew, setIsNew] = useState(false);
-  const [category, setCategory] = useState();
-
-  return <LoginScreen />;
+  return <AppFormField />;
 }
 
 const styles = StyleSheet.create({
